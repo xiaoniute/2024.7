b887fb6cb148d225c8dfe4341e997e4ad4c1b9eb
XmmShp <79584305+XmmShp@users.noreply.github.com>
2024-07-08 13:35:25 +0800

    feat: ResultWriter

--- src/Modules/ResultWriter.py
+++ src/Modules/ResultWriter.py
@@ -1,4 +1,18 @@
+import pandas
+
+
 class ResultWriter:
     @staticmethod
     def WriteDictToFile(value: dict, filePath: str) -> bool:
-        raise NotImplementedError
+        """
+        :param value: 被用来转换为Excel文件的字典
+        :param filePath: 输出到的Excel文件路径
+        :return: 是否成功输出
+        """
+        try:
+            df = pandas.DataFrame(value)
+            df.to_excel(filePath, index=False)
+            return True
+        except Exception as e:
+            print(e)
+            return False
