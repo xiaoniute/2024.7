f3b3c2606842244967c022871cf28acda73c429a
XmmShp <79584305+XmmShp@users.noreply.github.com>
2024-07-11 03:47:03 +0800

    feat: Trainer

---
+++ src/Train.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+from Utils.JsonHelper import JsonHelper
+
+if __name__ == "__main__":
+    config = JsonHelper.LoadDictFromFile("config.json")
+    print(config["pretraining-model-path"])
+    model = YOLO(model=config["pretraining-model-path"])
+    model.train(data=config["dataset-config-path"],
+                epochs=config["training-epoch"],
+                batch=config["training-batch"],
+                val=False
+                )
+    model.save(config["model-path"])
