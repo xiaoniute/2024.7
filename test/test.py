6f1b67d3120a117d817ea0279582c6676c3ec6da
zjuatri <zjuatri@qq.com>
2024-07-17 19:46:48 +0800

    feat: modify PlaneController

--- test/test.py
+++ test/test.py
@@ -1,23 +1,50 @@
 import cv2
 from ultralytics import YOLO
+import queue
+from collections import Counter
 
 def GetAnswer():
+    size = 60
     good_num = 0
     bad_num = 0
-    FrameQueue = []
+    FrameQueue = queue.Queue()
+    RecordQueue = queue.Queue()
+    RecordList = []
     model = YOLO("./model.pt")
-    image = cv2.imread("example1.jpg")    
-    result = model.predict(image, verbose=False, conf=0.88)
-    if True:
-        res = result[0].summary()
-        for target in res:
-            if target['class'] == 1:
-                bad_num+=1
-            if target['class'] == 0:
-                good_num+=1
-        recorder = (good_num,bad_num)
-        print(recorder)
+    image = cv2.imread("example2.jpg")    
+    confidence = 0.8
+    
+
+    while(FrameQueue.qsize() < size):
+        bad_num = 0
+        good_num = 0
+        FrameQueue.put(image)
+    
+        if 1:
+            result = model.predict(image, verbose=False, conf=0.88)
+            res = result[0].summary()
+            for target in res:
+                if target['class'] == 1:
+                    bad_num += 1
+                if target['class'] == 0:
+                    good_num += 1
+            recorder = (good_num,bad_num)
+            if good_num + bad_num <= 4:
+                RecordQueue.put(recorder)
+    
+    for _ in range(size):
+        record = RecordQueue.get()
+        RecordList.append(record)
+    element_counts =Counter(RecordList)
+    max_key = max(element_counts, key=element_counts.get)
+    rate = element_counts[max_key] / len(RecordList)
+    print(rate)
+    if rate >= confidence:
+        return max_key
+    else:
+        FrameQueue.get()
+        return GetAnswer()
 
     
 
-GetAnswer()
+print(GetAnswer())
